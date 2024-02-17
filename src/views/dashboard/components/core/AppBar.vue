208b3347392586955ffdd8584587265c296adb31
Munees <muneesmmm@gmail.com>
2024-02-17 15:43:04 +0530

    updated

--- src/views/dashboard/components/core/AppBar.vue
+++ src/views/dashboard/components/core/AppBar.vue
@@ -1,26 +1,9 @@
 <template>
-  <v-app-bar
-    id="app-bar"
-    absolute
-    app
-    color="transparent"
-    flat
-    height="75"
-  >
-    <v-btn
-      class="mr-3"
-      elevation="1"
-      fab
-      small
-      @click="setDrawer(!drawer)"
-    >
-      <v-icon v-if="value">
-        mdi-view-quilt
-      </v-icon>
+  <v-app-bar id="app-bar" absolute app color="transparent" flat height="75">
+    <v-btn class="mr-3" elevation="1" fab small @click="setDrawer(!drawer)">
+      <v-icon v-if="value"> mdi-view-quilt </v-icon>
 
-      <v-icon v-else>
-        mdi-dots-vertical
-      </v-icon>
+      <v-icon v-else> mdi-dots-vertical </v-icon>
     </v-btn>
 
     <v-toolbar-title
@@ -53,12 +36,7 @@
 
     <!-- <div class="mx-3" /> -->
 
-    <v-btn
-      class="ml-2"
-      min-width="0"
-      text
-      to="/"
-    >
+    <v-btn class="ml-2" min-width="0" text to="/">
       <v-icon>mdi-view-dashboard</v-icon>
     </v-btn>
 
@@ -118,66 +96,70 @@
 </template>
 
 <script>
-  // Components
-  import { VHover, VListItem } from 'vuetify/lib'
+// Components
+// import { VHover, VListItem } from "vuetify/lib";
 
-  // Utilities
-  import { mapState, mapMutations } from 'vuex'
+// Utilities
+import { mapState, mapMutations } from "vuex";
 
-  export default {
-    name: 'DashboardCoreAppBar',
+export default {
+  name: "DashboardCoreAppBar",
 
-    components: {
-      AppBarItem: {
-        render (h) {
-          return h(VHover, {
-            scopedSlots: {
-              default: ({ hover }) => {
-                return h(VListItem, {
-                  attrs: this.$attrs,
-                  class: {
-                    'black--text': !hover,
-                    'white--text secondary elevation-12': hover,
-                  },
-                  props: {
-                    activeClass: '',
-                    dark: hover,
-                    link: true,
-                    ...this.$attrs,
-                  },
-                }, this.$slots.default)
-              },
-            },
-          })
-        },
-      },
+  components: {
+    // AppBarItem: {
+    //   render(h) {
+    //     return h(VHover, {
+    //       scopedSlots: {
+    //         default: ({ hover }) => {
+    //           return h(
+    //             VListItem,
+    //             {
+    //               attrs: this.$attrs,
+    //               class: {
+    //                 "black--text": !hover,
+    //                 "white--text secondary elevation-12": hover,
+    //               },
+    //               props: {
+    //                 activeClass: "",
+    //                 dark: hover,
+    //                 link: true,
+    //                 ...this.$attrs,
+    //               },
+    //             },
+    //             this.$slots.default
+    //           );
+    //         },
+    //       },
+    //     });
+    //   },
+    // },
+  },
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
     },
+  },
 
-    props: {
-      value: {
-        type: Boolean,
-        default: false,
-      },
-    },
+  data: () => ({
+    notifications: [
+      "Mike John Responded to your email",
+      "You have 5 new tasks",
+      "You're now friends with Andrew",
+      "Another Notification",
+      "Another one",
+    ],
+  }),
 
-    data: () => ({
-      notifications: [
-        'Mike John Responded to your email',
-        'You have 5 new tasks',
-        'You\'re now friends with Andrew',
-        'Another Notification',
-        'Another one',
-      ],
+  computed: {
+    ...mapState(["drawer"]),
+  },
+
+  methods: {
+    ...mapMutations({
+      setDrawer: "SET_DRAWER",
     }),
-
-    computed: {
-      ...mapState(['drawer']),
-    },
-
-    methods: {
-      ...mapMutations({
-        setDrawer: 'SET_DRAWER',
-      }),
-    },
-  }
+  },
+};
 </script>
